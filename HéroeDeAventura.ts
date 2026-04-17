5b5e0063c20362f5a3bb6768bd45bda608f53740
yosoysosaaa8 <sosaorx90@gmail.com>
2026-04-16 23:07:51 -0500

    Actualizamos codigo

---
+++ HéroeDeAventura.ts
@@ -0,0 +1,31 @@
+class HéroeDeAventura{
+    nombre: string;
+    nivel: number;
+    estaVivo: boolean;
+    fechaCreacion: Date;
+    habilidades: string [];
+    estadisticas: string;
+
+    constructor(nombre: string, habilidades: string[]){
+        this.nombre = nombre;
+        this.nivel = 1;
+        this.estaVivo = true;
+        this.fechaCreacion = (new Date ());
+        this.habilidades = habilidades;
+        this.estadisticas = "100";
+
+    }
+    recibirDanio(puntos: number): void{
+        puntos=100-1;
+        if(puntos<=0){
+            this.estaVivo = false;
+        }
+    }
+    subirNivel(): void{
+        this.nivel = this.nivel + 1;
+        console.log(this.nombre+"ha subido al nivel"+this.nivel);
+    }
+    generarFicha(): string{
+        return "Heroe:"+this.nombre+"Nivel:"+this.nivel+"Estado:"+this.estaVivo;
+    }
+}
